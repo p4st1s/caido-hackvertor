b42007cacc1149d4402dccc5608442d3775eb45e
p4st1s <baptisterec@gmail.com>
2026-04-28 18:20:49 +0200

    ci: add release workflow with Ed25519 signing

--- caido.config.ts
+++ caido.config.ts
@@ -10,7 +10,7 @@ const id = "hackvertor";
 export default {
   id,
   name: "Hackvertor",
-  version: "0.0.1",
+  version: "0.2.0",
   description: "Tag-based data transformation and encoding toolkit",
   author: {
     name: "p4st1s"
